ea6698774d389bbb65b2022a64f0026ad5cfc9cc
jaibeer72 <jaibeer72@gmail.com>
2017-12-05 01:12:26 +0530

    Procedral generation with COlors :D done

---
+++ OpenGL-Main/TerrainLoading.cpp
@@ -0,0 +1,103 @@
+#include "TerrainLoading.h"
+
+TerrainLoading::TerrainLoading(const int w, const int d)
+{
+	width = w;
+	depth = d;
+
+	shader.LoadFromFile(GL_VERTEX_SHADER, "Tarrain.vert");
+	shader.LoadFromFile(GL_FRAGMENT_SHADER, "Tarrain.frag");
+	std::cout << "Load Sucessful";
+	shader.CreateAndLinkProgram();
+	std::cout << "Link sucessful";
+	shader.Use();
+	//add attributes and uniforms
+	shader.AddAttribute("vVertex");
+	shader.AddUniform("heightMapTexture");
+	shader.AddUniform("scale");
+	shader.AddUniform("half_scale");
+	shader.AddUniform("HALF_TERRAIN_SIZE");
+	shader.AddUniform("MVP");
+	//set values of constant uniforms as initialization	
+	glUniform1i(shader("heightMapTexture"), 0);
+	glUniform2i(shader("HALF_TERRAIN_SIZE"),w>>1,d>>1);
+	glUniform1f(shader("scale"), scale);
+	glUniform1f(shader("half_scale"), half_scale);
+	shader.UnUse();
+	std::cout << "SucessfulBind"; 
+	Init();
+}
+
+TerrainLoading::~TerrainLoading()
+{
+}
+
+int TerrainLoading::GetTotalVertices()
+{
+	
+	return (width*depth);
+}
+
+int TerrainLoading::GetTotalIndices()
+{
+	return (width*depth*2*3);
+}
+
+GLenum TerrainLoading::GetPrimitiveType()
+{
+	return GL_TRIANGLES;
+}
+
+void TerrainLoading::FillVertexBuffer(GLfloat * pBuffer)
+{
+	//PerlinNoise p(230);
+	FastNoise NG;
+	NG.SetNoiseType(FastNoise::PerlinFractal);
+	//NG.SetSeed(500);
+	NG.SetFractalOctaves(2);
+	NG.SetFrequency(0.01);
+	NG.SetFractalLacunarity(10.0);
+	
+	glm::vec3* vertices = (glm::vec3*)(pBuffer);
+	//setup vertices 
+	int count = 0;
+	//fill terrain vertices
+	for (int j = 0; j<depth; j++) {
+		for (int i = 0; i<width; i++) {
+			float x = ((float(i)) / (width - 1))*(width/2);
+			float z = (float(j)) / (depth - 1)*(depth/2);
+			
+			float y =20*NG.GetNoise(x, z);
+			
+			vertices[count] = glm::vec3(x,y,z);
+			count++;
+			//std::cout << "Y CORD OF PLAIN :- " << y << "\n";
+		}
+	}
+}
+
+void TerrainLoading::FillIndexBuffer(GLuint * pBuffer)
+{
+	GLuint* id = pBuffer;
+	for (int i = 0; i < depth - 1; i++) {
+		for (int j = 0; j < width - 1; j++) {
+			int i0 = j + i*width;
+			int i1 = i0 + 1;
+			int i2 = i0 + width;
+			int i3 = i2 + 1;
+			
+			*id++ = i0;
+			*id++ = i2;
+			*id++ = i1;
+			*id++ = i1;
+			*id++ = i2;
+			*id++ = i3;
+		}
+	}
+}
+
+void TerrainLoading::SetCustomUniforms()
+{
+
+}
+
--- OpenGL-Main/main.cpp
+++ OpenGL-Main/main.cpp
@@ -1,9 +1,11 @@
+
 // Headers and the objs 
-#include"Premitives2D.h"
 #include"OpenGL-Main.h"
 #include"GLSLShader.h"
 //#include"Premitives3D.h"
-
+#include"CFreeCamera.h"
+#include"CTexturedPlane.h"
+#include"TerrainLoading.h"
 
 #define _USE_MATH_DEFINES // M_PI constant
 #include<math.h>// This has to be declared after the define function NO CLUE WHy . 
@@ -11,39 +13,71 @@
 
 #define GL_CHECK_ERRORS assert(glGetError()== GL_NO_ERROR);
 
-
 OpenGLsetup OGL;
-Prem2D prem2D; 
 GLSLShader shader;
 //Premitives3D prem3D;
+CFreeCamera cam; 
+
+//When using multyple of the same kind of class :P 
+//YAY pointers finally 
+
+CTexturedPlane* checker_plane;
+TerrainLoading* Terrain; 
+
 //===============================
-// Demo Text Functions from the book 
+// Function Declaration
 //---------------------------=------
 
-
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void mouse_buttom_callback(GLFWwindow* window, int button, int action, int mods);
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+void OnInit();
+void framebuffer_size_callback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	cam.SetupProjection(45, (GLfloat)width / height);
+	
+}
+//void OnMouseDown(int button, int s, int x, int y);
 ///---------------------------------------------------------
 //-------------------------------
 //Global Variables
 //---------------------------
-const int Window_Width=1000;
-const int Window_Height=1000;
-//glmVertex vertices[3];
-GLuint vaoID;
-GLuint vboVerticesID;
-GLuint vboIndicesID;
+const int Window_Width=1920;
+const int Window_Height=1080;
+const float MOUSE_FILTER_WEIGHT = 0.75f;
+const int MOUSE_HISTORY_BUFFER_SIZE = 10;// ill figure this later
+glm::vec2 mouseHistory[MOUSE_HISTORY_BUFFER_SIZE];
+float mouseX = 0, mouseY = 0;
 
-const float SPEED = 2;
-float time = 0;
-const int NUM_X = 40;
-const int NUM_Z = 40;
-glm::vec3 vertices[(NUM_X + 1)*(NUM_Z + 1)];
-const int TOTAL_INDICES = NUM_X*NUM_Z * 2 * 3;
-GLushort indices[TOTAL_INDICES];
-float rX = 25, rY = -40, dist = -7;
-const float SIZE_X = 4; //size of plane in world space
-const float SIZE_Z = 4;
-const float HALF_SIZE_X = SIZE_X / 2.0f;
-const float HALF_SIZE_Z = SIZE_Z / 2.0f;
+//flag to enable filtering
+bool useFiltering = true;
+
+
+const std::string filename = "heightMap.jpg";
+// Camera Rotation Values
+//GLfloat alpha = 210.0f, beta = -70.0f, zoom = 2.0f;
+
+
+//for floating point imprecision
+const float EPSILON = 0.001f;
+const float EPSILON2 = EPSILON*EPSILON;
+double mouse_sensitivity = 0.5;
+//camera tranformation variables
+int state = 0, oldX = 0, oldY = 0;
+float rX = 0, rY = 0, fov = 45;
+//delta time
+float dt = 0;
+
+//timing related variables
+float last_time = 0, current_time = 0;
+
+
+
+// Texture Variable 
+GLuint heightMapTextureID;
 
 ///------------------------------------------
 //------------------------------------
@@ -54,48 +88,152 @@ glm::mat4  P = glm::mat4(1); // projection Mat
 glm::mat4 MV = glm::mat4(1); // Model Mat
 
 
-
-
-
 void main() {
 	GLFWwindow* window = OGL.CreateWindow(Window_Width, Window_Height, "This Is a Window Name");
   //GLFWwindow* window = OGL.CreateWindow(Window_Width, Window_Height, "This Is a Window Name");
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
 	OGL.CheckWindowWorking(window); 
 	OGL.BasicAntiAlasing();
-	glfwMakeContextCurrent(window);
-	
 	//glfwSwapInterval(1);
-	
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+	glewInit();
+	// Keycallbacks
+	glfwSetKeyCallback(window, key_callback); 
+	//glfwSetMouseButtonCallback(window, mouse_buttom_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	framebuffer_size_callback(window, width, height);
+		
 	//start code here
 	
-
-	
-	
+	OnInit();
+	//glm::vec3 vertices[4];
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(5.0f);
 	while (!glfwWindowShouldClose(window))
 	{
-		OGL.Ortho_Projection_Setup(window, Window_Width, Window_Height);
 		
-		//OGL.PrespectiveCamera_Setup(window, Window_Width, Window_Height); 	
-		//--------------------------
-		// enter code here 
-		//------------------------
-		//glViewport(0, 0, (GLsizei)Window_Width, (GLsizei)Window_Height);
-		//prem2D.drawTriangle(vertices[0], vertices[1], vertices[2]);
-	//	prem3D.drawReppleMesh(5, 5); 
+		glfwPollEvents();
+		last_time = current_time;
+		current_time = glfwGetTime()/ 1000.0f*1500.0f;
+		dt = current_time - last_time;
+
+		//clear color buffer and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
 		
+		//set the camera transformation
+		glm::mat4 MV = cam.GetViewMatrix();
+		glm::mat4 P = cam.GetProjectionMatrix();
+		glm::mat4 MVP = P*MV;
+
+		//render the chekered plane
+		//checker_plane->Render(glm::value_ptr(MVP));
+		Terrain->Render(glm::value_ptr(MVP));
+
 		//-----------------------------
 		//end code 
 		//------------------------------
 		glfwSwapBuffers(window);
-		glfwPollEvents();
+		
 	}
 	shader.DeleteShaderProgram();
-	//prem2D.deleteBufferObjs();
-	//prem3D.deleteBufferObjs(); 
+	delete checker_plane;
+	glDeleteTextures(1, &heightMapTextureID);
 	glfwDestroyWindow(window);
   glfwTerminate();
   exit(EXIT_SUCCESS);
 }
 
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods)
+{
+	if (GLFW_PRESS==true) {
 
+		switch (key)
+		{
+			
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			break;
+		case GLFW_KEY_W:
+			cam.Walk(dt);
+			break;
+		case GLFW_KEY_S:
+			cam.Walk(-dt);
+			break;
+		case GLFW_KEY_A:
+			cam.Strafe(-dt);
+			break;
+		case GLFW_KEY_D:
+			cam.Strafe(dt);
+			break;
+		case GLFW_KEY_Q:
+			cam.Lift(dt);
+			break;
+		case GLFW_KEY_Z:
+			cam.Lift(-dt);
+			break;
+		}
+	}
+		glm::vec3 t = cam.GetTranslation();
+		if (glm::dot(t, t) > EPSILON2) {
+			cam.SetTranslation(t*0.95f);
+		
+	}
+}
+
+
+
+
+void cursor_position_callback(GLFWwindow * window, double xpos, double ypos)
+{
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		 mouseX = -xpos*mouse_sensitivity;
+		 mouseY = ypos*mouse_sensitivity;
+	 //}
+	 cam.Rotate(mouseX, mouseY, 0);
+ }
+
+
+
+void OnInit() {
+	GL_CHECK_ERRORS
+		//generate the checker texture
+	/*	GLubyte data[128][128] = { 0 };
+	for (int j = 0; j<128; j++) {         Use this logic to generate noise maps :D 
+		for (int i = 0; i<128; i++) {
+			data[i][j] = (i <= 64 && j <= 64 || i>64 && j>64) ? 255 : 0;
+		}
+	}*/
+		Terrain = new TerrainLoading();
+		int texture_width = 0, texture_height = 0, channels = 0;
+	GLubyte* pData = SOIL_load_image(filename.c_str(), &texture_width, &texture_height, &channels, SOIL_LOAD_L);
+
+	//vertically flip the heightmap image on Y axis since it is inverted 
+	
+	//setup OpenGL texture
+	
+	
+
+		//setup camera
+		//setup the camera position and look direction
+		glm::vec3 p = glm::vec3(5);
+	cam.SetPosition(p);
+	glm::vec3 look = glm::normalize(p);
+
+	//rotate the camera for proper orientation
+	//float yaw = glm::degrees(float(atan2(look.z, look.x) + M_PI));
+	//float pitch = glm::degrees(asin(look.y));
+	//rX = yaw;
+	//rY = pitch;
+	//if (useFiltering) {
+	//	for (int i = 0; i < MOUSE_HISTORY_BUFFER_SIZE; ++i) {
+	//		mouseHistory[i] = glm::vec2(rX, rY);
+	//	}
+	//}
+	//cam.Rotate(rX, rY, 0);
+	std::cout << "Initialization successfull" << std::endl;
+}
